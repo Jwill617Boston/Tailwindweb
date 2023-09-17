5bed916a43c4e5e26dae9338057efb4e2aadcffd
JwillSDP <jwilliams@souldopepro.com>
2023-09-17 03:00:13 -0400

    Home page with sections

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function NavBar() {
    return (
       <nav className="sticky w-full bg-red-800 h-16">
-         <ul className="flex justify-center space-x-4">
-            <li>
+         <ul className="flex justify-center justify-evenly space-x-4 py-5">
+            <li className="">
                <Link href="/">Home</Link>
             </li>
             <li>
--- app/page.js
+++ app/page.js
@@ -1,11 +1,19 @@
-import Image from 'next/image'
-
-
 export default function Home() {
    return (
       <>
-         <div className="container flex mx-auto h-screen bg-slate-300">
-            <h1>Home Page</h1>
+         <div className="container mx-auto h-screen bg-slate-300">
+            <section className="flex justify-center  h-full bg-orange-500">
+               <h1>Section 1</h1>
+            </section>
+            <section className="flex justify-center  h-full bg-orange-100">
+               <h1>Section 2</h1>
+            </section>
+            <section className="flex justify-center  h-full bg-orange-800">
+               <h1>Section 3</h1>
+            </section>
+            <section className="flex justify-center  h-full bg-orange-500">
+               <h1>Section 4</h1>
+            </section>
          </div>
       </>
    );
