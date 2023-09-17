9dfb44958abe62ce130fb67d8681c3046d210faa
JwillSDP <jwilliams@souldopepro.com>
2023-09-17 14:31:48 -0400

    Nave home page

--- app/page.js
+++ app/page.js
@@ -3,5 +3,9 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-   return <div className="flex"></div>;
+   return (
+      <div className="container mx-auto flex justify-center items-center p-20">
+         <h1>Home Page</h1>
+      </div>
+   );
 }
